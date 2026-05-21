bb24263d8b3b283a17e388e057a82a99b6b7d165
waliwuao <1276672206@qq.com>
2026-05-21 22:54:40 +0800

    fix: write OSC 133 after oh-my-zsh install to avoid being overwritten

--- install.sh
+++ install.sh
@@ -39,70 +39,25 @@ registry = "sparse+https://mirrors.tuna.tsinghua.edu.cn/crates.io-index/"
 EOF
 echo "Cargo mirror set to Tsinghua"
 
-echo "==> 4. Configure zsh"
-if [ "$ZSH_WAS_NEW" = true ]; then
-    cat > "$ZSH_CONFIG" << 'ZSHEOF'
-# zsh configuration
-
-# History
-HISTSIZE=50000
-SAVEHIST=50000
-HISTFILE=~/.zsh_history
-setopt EXTENDED_HISTORY
-setopt INC_APPEND_HISTORY
-setopt SHARE_HISTORY
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_FIND_NO_DUPS
-setopt HIST_SAVE_NO_DUPS
-
-# Completion
-autoload -Uz compinit
-compinit
-zstyle ':completion:*' menu select
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-# Aliases
-alias ll='ls -alFh'
-alias la='ls -Ah'
-alias l='ls -CFh'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias grep='grep --color=auto'
-
-# OSC 133 shell integration (Alacritty command navigator)
-preexec() { print -Pn "\e]133;C;\e\\" }
-precmd()  { print -Pn "\e]133;A;\e\\" }
-
-# Prompt
-autoload -Uz promptinit
-promptinit
-prompt adam1
-
-# PATH
-export PATH="$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
-ZSHEOF
-    echo "zshrc created: $ZSH_CONFIG"
-else
-    if ! grep -q "OSC 133" "$ZSH_CONFIG" 2>/dev/null; then
-        cat >> "$ZSH_CONFIG" << 'ZSHEOF'
-
-# OSC 133 shell integration (Alacritty command navigator)
-preexec() { print -Pn "\e]133;C;\e\\" }
-precmd()  { print -Pn "\e]133;A;\e\\" }
-ZSHEOF
-        echo "OSC 133 appended to: $ZSH_CONFIG"
-    else
-        echo "OSC 133 already present in zshrc"
-    fi
-fi
-
-echo "==> 5. Install Oh My Zsh"
+echo "==> 4. Install Oh My Zsh"
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(wget -O- https://install.ohmyz.sh/)"
     echo "Oh My Zsh installed"
 else
     echo "Oh My Zsh already installed"
+fi
+
+echo "==> 5. Configure OSC 133 shell integration"
+if ! grep -q "OSC 133" "$ZSH_CONFIG" 2>/dev/null; then
+    cat >> "$ZSH_CONFIG" << 'ZSHEOF'
+
+# OSC 133 shell integration (Alacritty command navigator)
+preexec() { print -Pn "\e]133;C;\e\\" }
+precmd()  { print -Pn "\e]133;A;\e\\" }
+ZSHEOF
+    echo "OSC 133 appended to: $ZSH_CONFIG"
+else
+    echo "OSC 133 already present in zshrc"
 fi
 
 echo ""
